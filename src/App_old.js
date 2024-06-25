5fdb8714fc5dc53647c6dd342ef24b45d38f03e0
Alexander Putilin <avp-13@yandex.ru>
2024-06-25 15:34:10 +0100

    Fix full results table

--- src/App_old.js
+++ src/App_old.js
@@ -1422,14 +1422,53 @@ function App() {
                           <tbody>
                             {partiesExtendedTableItems.map((item, index) => (
                               <tr key={index}>
-                                {partiesTableColumns.map((column) => (
-                                  <td
-                                    key={column}
-                                    dangerouslySetInnerHTML={{
-                                      __html: item[column],
-                                    }}
-                                  ></td>
-                                ))}
+                                {partiesTableColumns.map((column) => {
+                                  switch (column) {
+                                    case "name":
+                                      return (
+                                        <td
+                                          key={column}
+                                          dangerouslySetInnerHTML={{
+                                            __html: item[column],
+                                          }}
+                                        />
+                                      );
+                                    case "totalVotes":
+                                    case "totalVotesPerSeat":
+                                      return (
+                                        <td key={column}>
+                                          {commas(item[column])}
+                                        </td>
+                                      );
+                                    case "totalVotesShare":
+                                      return (
+                                        <td key={column}>
+                                          {!oneDecimal(item[column]) ? (
+                                            <>
+                                              <span
+                                                style={{ fontFamily: "Arial" }}
+                                              >
+                                                &lt;
+                                              </span>
+                                              <span> 0.1</span>
+                                            </>
+                                          ) : (
+                                            oneDecimal(item[column])
+                                          )}
+                                        </td>
+                                      );
+                                    case "totalSeatsShare":
+                                      return (
+                                        <td key={column}>
+                                          {oneDecimal(item[column])}
+                                        </td>
+                                      );
+                                    default:
+                                      return (
+                                        <td key={column}>{item[column]}</td>
+                                      );
+                                  }
+                                })}
                               </tr>
                             ))}
                           </tbody>
